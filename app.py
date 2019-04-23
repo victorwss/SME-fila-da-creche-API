814f269db54745bd48b27c96fc9b4fa507c36e68
victorwss <victorwssilva@gmail.com>
2019-04-23 16:36:52 -0300

    Correção de problemas de segurança e gerenciamento da conexão com o banco de dados.

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 # -*- coding: UTF-8 -*-
 
-import psycopg2
-from psycopg2.extras import RealDictCursor
 import os
-from flask import Flask, jsonify, abort, request, make_response, url_for
+from flask import Flask, jsonify, abort, request, url_for
+from wrap_connection import transact
 # from flask_httpauth import HTTPBasicAuth
-from tenacity import retry, wait_fixed
-from geojson import Feature, Point, FeatureCollection
+
+#from geojson import Feature, Point, FeatureCollection
 
 POSTGRES_USER = os.getenv("POSTGRES_USER")
 POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
@@ -28,130 +27,100 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 #     return None
 # @auth.error_handler
 # def unauthorized():
-#     return make_response(jsonify( { 'error': 'Unauthorized access' } ), 403)
+#     return jsonify( { 'error': 'Unauthorized access' } ), 403
 #     # return 403 instead of 401 to prevent browsers from displaying the default auth dialog
 
-# wait to see if db is up, 5 seconds between retries
-@retry(wait=wait_fixed(5))
-def init_api():
-    try:
-        con = psycopg2.connect(dbname=POSTGRES_DBNAME, user=POSTGRES_USER, host=POSTGRES_HOST, password=POSTGRES_PASSWORD)
-        con.set_session(autocommit=True)
-    except Exception as e:
-        print("I am unable to connect to the database.")
-        print(e)
-        abort(400)
+def connect():
+    import psycopg2
+    return psycopg2.connect(
+        dbname = POSTGRES_DBNAME,
+        user = POSTGRES_USER,
+        host = POSTGRES_HOST,
+        password = POSTGRES_PASSWORD
+    )
 
-    cur = con.cursor(cursor_factory = psycopg2.extras.RealDictCursor)
+def make_cursor(connection):
+    import psycopg2
+    return connection.cursor(cursor_factory = psycopg2.extras.RealDictCursor)
 
-    # 2000 meters
-    searchRadius = 2000
+make_connection = { "db_connect" : connect, "cursor_factory": make_cursor }
 
-    @app.errorhandler(400)
-    def not_found(error):
-        return make_response(jsonify( { 'error': 'Bad request' } ), 400)
+# 2000 meters
+searchRadius = 2000
 
-    @app.errorhandler(404)
-    def not_found(error):
-        return make_response(jsonify( { 'error': 'Not found' } ), 404)
+@app.errorhandler(400)
+def bad_request(error):
+    return jsonify( { 'error': 'Bad request' } ), 400
 
-    @app.route('/', methods = ['GET'])
-    # @auth.login_required
-    def get_hello():
-        return 'Hello World.'
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify( { 'error': 'Not found' } ), 404
 
-    @app.route('/v1/schools/id/<string:cd_unidade_educacao>', methods = ['GET'])
-    # sample url
-    # http://localhost:8080/v1/schools/id/091383
-    # @auth.login_required
-    def get_school_id(cd_unidade_educacao):
-        try:
-            if validate_school_id_request(cd_unidade_educacao):
-                cur.execute(f"""
-                SELECT *
-                FROM unidades_educacionais_ativas_endereco_contato
-                WHERE cd_unidade_educacao = '{cd_unidade_educacao}'
-                """)
-                schools = cur.fetchall()
-                return jsonify( { 'results': schools } )
-            else:
-                abort(400)
-        except Exception as e:
-            print(e)
-            abort(400)
+@app.route('/', methods = ['GET'])
+# @auth.login_required
+def get_hello():
+    return 'Hello World.'
 
-    @app.route('/v1/schools/radius/<string:lon>/<string:lat>', methods = ['GET'])
-    # sample url
-    # http://localhost:8080/v1/schools/radius/-46.677023599999984/-23.5814295
-    # @auth.login_required
-    def get_schoolradius(lat, lon):
-        try:
-            if validate_wait_request(lat, lon):
-                cur.execute(f"""SELECT *
-                FROM unidades_educacionais_ativas_endereco_contato
-                WHERE ST_DWithin(geom::geography, ST_SetSRID(ST_MakePoint({lon}, {lat}), 4326), {searchRadius})
-                """)
-                rowsSchools = cur.fetchall()
-                return jsonify( { 'results': rowsSchools } )
-            else:
-                abort(400)
-        except Exception as e:
-            print(e)
-            abort(400)
+@transact(**make_connection)
+@app.route('/v1/schools/id/<int:cd_unidade_educacao>', methods = ['GET'])
+# sample url
+# http://localhost:8080/v1/schools/id/091383
+# @auth.login_required
+def get_school_id(cd_unidade_educacao):
+    cursor.execute("SELECT * FROM unidades_educacionais_ativas_endereco_contato WHERE cd_unidade_educacao = %d", (cd_unidade_educacao,))
+    return jsonify( { 'results': cursor.fetchall() } )
 
+@transact(**make_connection)
+@app.route('/v1/schools/radius/<float:lon>/<float:lat>', methods = ['GET'])
+# sample url
+# http://localhost:8080/v1/schools/radius/-46.677023599999984/-23.5814295
+# @auth.login_required
+def get_schoolradius(lat, lon):
+    if not validate_coord(lat, lon):
+        abort(422)
+    cursor.execute("SELECT * FROM unidades_educacionais_ativas_endereco_contato WHERE ST_DWithin(geom::geography, ST_SetSRID(ST_MakePoint(%s, %s), 4326), %s)", (lon, lat, searchRadius))
+    return jsonify( { 'results': cursor.fetchall() } )
 
-    @app.route('/v1/schools/radius/wait/<string:lon>/<string:lat>/<int:cd_serie>', methods = ['GET'])
-    # sample url
-    # http://localhost:8080/v1/schools/radius/wait/-46.677023599999984/-23.5814295/27
-    # @auth.login_required
-    def get_schoolradiuswait(lat, lon, cd_serie):
-        try:
-            # FIXME: validate by bouding box too
-            if validate_wait_request(lat, lon, cd_serie):
-                cur.execute(f"""
-                SELECT *, (ST_Distance(geom::geography, ST_SetSRID(ST_MakePoint({lon}, {lat}), 4326)) / 1000) as distance FROM unidades_educacionais_ativas_endereco_contato AS u
-                LEFT JOIN unidades_educacionais_infantil_vagas_serie as v
-                ON u.cd_unidade_educacao = v.cd_unidade_educacao
-                WHERE ST_DWithin(geom::geography, ST_SetSRID(ST_MakePoint({lon}, {lat}), 4326), {searchRadius})
-                  AND v.vagas_cd_serie_{cd_serie} IS NOT NULL
-                ORDER BY distance
-                """)
-                rowsSchools = cur.fetchall()
-                cur.execute(f"""
-                SELECT count(DISTINCT cd_solicitacao_matricula_random)
-                FROM unidades_educacionais_ativas_endereco_contato AS u
-                LEFT JOIN solicitacao_matricula_grade_dw AS s
-                ON u.cd_unidade_educacao::integer = s.cd_unidade_educacao
-                WHERE ST_DWithin(geom::geography, ST_SetSRID(ST_MakePoint({lon}, {lat}), 4326), {searchRadius})
-                  AND s.cd_serie_ensino = {cd_serie}
-                """)
-                rowsWait = cur.fetchall()
-                cur.execute(f"""
-                SELECT dt_solicitacao as updated_at
-                FROM solicitacao_matricula_grade_dw_atualizacao
-                """)
-                rowsUpdated = cur.fetchall()
-                results = {'wait': rowsWait[0]['count'], 'wait_updated_at': rowsUpdated[0]['updated_at'], 'schools': rowsSchools}
-                return jsonify( { 'results': results } )
-            else:
-                abort(400)
-        except Exception as e:
-            print(e)
-            abort(400)
+@transact(**make_connection)
+@app.route('/v1/schools/radius/wait/<float:lon>/<float:lat>/<int:cd_serie>', methods = ['GET'])
+# sample url
+# http://localhost:8080/v1/schools/radius/wait/-46.677023599999984/-23.5814295/27
+# @auth.login_required
+def get_schoolradiuswait(lat, lon, cd_serie):
+    if cd_serie not in [1, 4, 27, 28] or not validate_coord(lat, lon):
+        abort(422)
+    # FIXME: validate by bouding box too
 
-    def validate_wait_request(lat, lon, cd_serie=1):
-        if float(lat) and float(lon) and (cd_serie in [1, 4, 27, 28]):
-            return True
-        else:
-            return False
+    sql1 = """
+    SELECT *, (ST_Distance(geom::geography, ST_SetSRID(ST_MakePoint(%d, %d), 4326)) / 1000) AS distance
+    FROM unidades_educacionais_ativas_endereco_contato AS u
+    LEFT JOIN unidades_educacionais_infantil_vagas_serie AS v ON u.cd_unidade_educacao = v.cd_unidade_educacao
+    WHERE ST_DWithin(geom::geography, ST_SetSRID(ST_MakePoint(%d, %d), 4326), %d)
+    AND v.vagas_cd_serie_{cd_serie} IS NOT NULL
+    ORDER BY distance
+    """
+    cursor.execute(sql1, (lon, lat, lon, lat, searchRadius))
+    rowsSchools = cur.fetchall()
 
-    def validate_school_id_request(id):
-        if float(id):
-            return True
-        else:
-            return False
+    sql2 = """
+    SELECT COUNT(DISTINCT cd_solicitacao_matricula_random) AS cnt
+    FROM unidades_educacionais_ativas_endereco_contato AS u
+    LEFT JOIN solicitacao_matricula_grade_dw AS s ON u.cd_unidade_educacao::integer = s.cd_unidade_educacao
+    WHERE ST_DWithin(geom::geography, ST_SetSRID(ST_MakePoint(%d, %d), 4326), %d)
+      AND s.cd_serie_ensino = %d
+    """
+    cursor.execute(sql2, (lon, lat, seachRadius, cd_serie))
+    rowsWait = cur.fetchone()
 
-init_api()
+    sql3 = "SELECT dt_solicitacao AS updated_at FROM solicitacao_matricula_grade_dw_atualizacao"
+    cursor.execute(sql3)
+    rowsUpdated = cur.fetchone()
+
+    results = {'wait': rowsWait['cnt'], 'wait_updated_at': rowsUpdated['updated_at'], 'schools': rowsSchools}
+    return jsonify( { 'results': results } )
+
+def validate_coord(lat, lon):
+    return type(lat) in [int, float] and type(lon) in [int, float] and lat >= -90 and lat <= +90 and lon >= -180 and lon <= +180
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
